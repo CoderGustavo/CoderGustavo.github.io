a2d710d7b5ee979f5a5bad98cee9b1cb185f9c3c
Gustavo Ornaghi Antunes <gustavoornaghiantunes@gmail.com>
2021-08-05 10:05:26 -0300

    mudar cor no navbar ao rodar a pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,6 +77,37 @@ $(function(){
             scrollTop: targetOffset - 100
         }, 100);
     });
+    $('.menu a[href="#inicio"]').click();
+
+    const altura_sobre = $("#sobre").offset().top;
+    const altura_servicos = $("#servicos").offset().top;
+    const altura_portifolio = $("#portifolio").offset().top;
+    const altura_contato = $("#contato").offset().top;
+    
+    $(document).on("scroll", function(){
+        var altura_documento = ($(window).scrollTop())+150;
+        if(altura_documento < altura_sobre){
+            $('.menu a[href="#inicio"]').addClass("active");
+            $('.menu a[href="#inicio"]').siblings().removeClass("active");
+        }
+        if(altura_documento >= altura_sobre && altura_documento <= altura_servicos){
+            $('.menu a[href="#sobre"]').addClass("active");
+            $('.menu a[href="#sobre"]').siblings().removeClass("active");
+        }
+        if(altura_documento >= altura_servicos && altura_documento <= altura_portifolio){
+            $('.menu a[href="#servicos"]').addClass("active");
+            $('.menu a[href="#servicos"]').siblings().removeClass("active");
+        }
+        if(altura_documento >= altura_portifolio && altura_documento <= altura_contato){
+            $('.menu a[href="#portifolio"]').addClass("active");
+            $('.menu a[href="#portifolio"]').siblings().removeClass("active");
+        }
+        if(altura_documento >= altura_contato){
+            $('.menu a[href="#contato"]').addClass("active");
+            $('.menu a[href="#contato"]').siblings().removeClass("active");
+        }
+    });
+    
 
     $('.filtros-portifolio a').on('click', function(e){
         e.preventDefault();
@@ -90,5 +121,6 @@ $(function(){
             $("#imagens-portifolio div[filtro="+filtro+"]").show();
         }
     });
+
     $('.filtros-portifolio a[filtrar="todos"]').click();
 });
